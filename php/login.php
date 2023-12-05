623bd0061e6ef2d16b05b425ba34dc0cad37c474
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-12-05 22:39:03 +0530

    Add files via upload

---
+++ php/login.php
@@ -0,0 +1,42 @@
+<?php
+
+$conn = mysqli_connect("localhost", "root", "", "final_db");
+session_start();
+
+if (isset($_SESSION["id"])) {
+    header ("Location: profile.php");
+}
+
+if(isset($_POST["login"])){
+    global $conn;
+
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $sql = "SELECT * FROM users WHERE Email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+    if(mysqli_num_rows($result) > 0){
+
+        $row = mysqli_fetch_assoc($result);
+
+        if(password_verify($password, $user["Password"])){
+            header("Location: profile.php");
+            $_SESSION['firstname'] = $user["Firstname"];
+            $_SESSION['lastname'] = $user["Lastname"];
+            $_SESSION['email'] = $user["Email"];
+            $_SESSION['phone'] = $user["Phone"];
+            $_SESSION['age'] = $user["Age"];
+            die();
+        }
+        else{
+        echo "Wrong Password";
+        exit;
+        }
+    }
+    else{
+        echo "User Not Registered";
+        exit;
+    }
+}
---
+++ php/profile.php
@@ -0,0 +1,57 @@
+<?php
+
+$currentYear = date("Y");
+
+session_start();
+$firstname = $_SESSION['firstname'];
+$lastname = $_SESSION['lastname'];
+$email = $_SESSION['email'];
+$phone = $_SESSION['phone'];
+$age = $_SESSION['age'];
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/index.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+	<title>Home</title>
+
+</head>
+
+<body class="container-fluid bg-secondary">
+
+        <div class="row bg-light">
+            <span class="col-lg-8 mt-1"><h2><a href="#" class="point">LOGO</a></h2></span>
+            <span class="col-lg-2 mb-3 mt-3">
+                <a href="#" class="point">edit profile</a>
+            </span>
+            <span class="col-lg-2 mt-2 mb-2">
+                <button type="button" class="round btn btn-primary">Log Out</button>
+            </span>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-lg-4 rounded col-lg-4 bg-light m-auto wrapper" >
+
+                <form action="php/login.php" method="post">
+
+                    <p style="font-size: 22px;" class="text-center bg-light mt-3 mb-3">Hii <b><i><?php echo $firstname ?></i></b>, Welcome to the page.</p>
+                    <p style="font-size: 22px;" class="text-center bg-light mt-3 mb-3">Your e-mail is <b><i><?php echo $email ?></i></b>.</p>
+                    <p style="font-size: 22px;" class="text-center bg-light mt-3 mb-3">Your Phone Number is <b><i><?php echo $phone ?></i></b>.</p>
+                    <p style="font-size: 22px;" class="text-center bg-light mt-3 mb-3">You are <b><i><?php echo $currentYear-$age ?></i></b> years old.</p>
+
+                    
+                </form>
+
+            </div>
+        </div>
+</body>
+
+</html>
